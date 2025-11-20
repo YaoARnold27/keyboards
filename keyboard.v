e6e9b7a243b73109036ebc6b701b7f322651f528
ChengjiYao <yaoarnold27@gmail.com>
2025-11-20 05:53:29 -0500

    send

--- keyboard.v
+++ keyboard.v
@@ -16,21 +16,26 @@ module keyboard (CLOCK_50, KEY,SW, PS2_CLK, PS2_DAT, LEDR, HEX0, HEX1, HEX2, HEX
     wire Resetn;
     assign Resetn = KEY[0];
     wire [23:0] datout;
-
+    wire [23:0] datout_2;
+    wire a,b,f,A,B,F,CLOCK_OUT;
     
     get_key gt (CLOCK_50,Resetn,PS2_CLK, PS2_DAT,datout);
+    ISTYPE il (datout[7:0],a,b,f);
+    assign LEDR[3] = a;
+    assign LEDR[4] = b;
+    assign LEDR[5] = f;
+    FSM fsm(a,b,f,Resetn,CLOCK_50,datout,datout_2,LEDR[1]);
+    
 
 
 
+    hex7seg aa (datout[3:0],HEX0);
+    hex7seg bb (datout[7:4],HEX1);
 
-    ISTYPE il (datout[7:0],LEDR[1],LEDR[2]);
-
-    hex7seg a (datout[3:0],HEX0);
-    hex7seg b (datout[7:4],HEX1);
-    hex7seg c (datout[11:8],HEX2);
-    hex7seg d (datout[15:12],HEX3);
-    hex7seg e (datout[19:16],HEX4);
-    hex7seg f (datout[23:20],HEX5);
+    // hex7seg c (datout[11:8],HEX2);
+    // hex7seg d (datout[15:12],HEX3);
+    // hex7seg e (datout[19:16],HEX4);
+    // hex7seg f (datout[23:20],HEX5);
 
 
 
@@ -52,7 +57,6 @@ module get_key(CLOCK_50,Resetn,PS2_CLK, PS2_DAT,DAT_OUT);
     assign negedge_ps2_clk = (prev_ps2_clk & !PS2_CLK);
 
 
-
     always @(posedge CLOCK_50) begin
         if (Resetn == 0) 
             DAT <= 11'b00111100000;
@@ -71,13 +75,15 @@ module get_key(CLOCK_50,Resetn,PS2_CLK, PS2_DAT,DAT_OUT);
     assign DAT_OUT[23:20] = DAT[30:27];
 endmodule
 
-module ISTYPE(KEY_8,FLAG1,FLAG2);
+module ISTYPE(KEY_8,FLAG1,FLAG2,FLAG3);
     input wire [7:0] KEY_8;
     output reg FLAG1;
     output reg FLAG2;
+    output reg FLAG3;
     always @(*) begin
         FLAG1 = 0;
         FLAG2 = 0;
+        FLAG3 = 0;
         case (KEY_8)
             8'h1C, 8'h32, 8'h21, 8'h23, 8'h24, 8'h2B, 
             8'h34, 8'h33, 8'h43, 8'h3B, 8'h42, 8'h4B,
@@ -87,9 +93,71 @@ module ISTYPE(KEY_8,FLAG1,FLAG2);
                 FLAG1 = 1;
             8'h0C:
                 FLAG2 = 1;
+            8'hF0:
+                FLAG3 = 1;
 
         endcase
     end
+endmodule
+
+
+module FSM(A,B,F,RESETN,CLOCK,DATA,DATA_OUT,SHIFT);
+input wire A;
+input wire B;
+input wire F;
+input wire RESETN;
+input wire CLOCK;
+input wire [7:0] DATA;
+output wire [7:0] DATA_OUT;
+output wire SHIFT;
+
+reg [3:0] y;
+reg [3:0] Y;
+reg [7:0] SW;
+reg  SHIFTSW;
+
+
+parameter CR=4'h0, PR=4'h1 , RL=4'h2, SH = 4'h3,Q=4'h4;
+always @(*)
+    case (y)
+        CR:begin
+            if(F) Y=RL;
+            else if(B) Y=SH;
+            else if (A) Y=PR;
+            
+        end
+        PR: begin
+            if (F) Y=RL;
+            else if (A) Y = PR;
+            SW = DATA;
+        end
+        SH: begin
+        if(B) Y=SH;
+            else if (A) Y=Q;
+        end
+        Q: begin 
+            if (A) Y=Q;
+            else if (F) Y=RL;
+            SW = DATA;
+            SHIFTSW = 1;
+        end
+        RL: begin 
+        if ((A|B)) Y=CR;
+        end
+
+
+        default: begin 
+        SHIFTSW = 0;
+        SW = 8'b0;
+        end
+    endcase
+
+always @(posedge CLOCK)
+    if (!RESETN) begin y <= CR; SW <=8'b0; SHIFTSW <=0;end
+    else y<=Y;
+
+assign SHIFT = SHIFTSW;
+assign DATA_OUT = SW;
 endmodule
 
 
